7e30e9fc9b2c289d4af2028c6588032c313deb4b
Jamik <dzamikhidirov8@gmail.com>
2026-02-20 18:52:10 +0300

    добавлен сплеш скрин

--- app/src/main/java/com/example/dentalfirst/MainActivity.kt
+++ app/src/main/java/com/example/dentalfirst/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.dentalfirst.ui.theme.DentalFirstTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +20,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        setContent {
+        val splashScreen = installSplashScreen()
 
+        setContent {
+            MainScreen()
         }
     }
 }
 
+
+
+@Composable
+fun MainScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+}
